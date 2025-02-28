9f7610a197f43c581ba905306c75d489a7742415
HaiBV <buivuhai.bk@gmail.com>
2025-02-28 08:46:00 +0700

    chore: remove unuse project

--- fetch-with-vite-react-ts/src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { ComponentProps } from "react";
-
-const Button = (props: Omit<ComponentProps<"button">, "className"> & { className: number }) => {
-  console.log(props);
-  return <div>Button</div>;
-};
-
-export default Button;
--- fetch-with-vite-react-ts/src/utils/env.ts
+++
@@ -1,9 +0,0 @@
-const PREFIX = "VITE_";
-
-export const ENV_VAR_KEYS = ["APP_TITLE", "JOKE_API_BASE_URL", "JOKE_API_KEY", "JOKE_API_HOST"] as const;
-
-const env = (key: (typeof ENV_VAR_KEYS)[keyof typeof ENV_VAR_KEYS]): string => {
-  return import.meta.env[`${PREFIX}${key}`];
-};
-
-export default env;
