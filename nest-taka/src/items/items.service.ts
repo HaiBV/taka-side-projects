369cf6c66914720814174fe77ec70e94b121a2fb
HaiBV <buivuhai.bk@gmail.com>
2025-07-05 22:31:19 +0700

    feat: update RUD item repo

--- nest-taka/src/items/items.service.ts
+++ nest-taka/src/items/items.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { EntityManager, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
-import { EntityManager } from 'typeorm';
-import { Item } from './entities/item.entity';
 
 @Injectable()
 export class ItemsService {
-  constructor(private readonly entityManager: EntityManager) {}
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemRepository: Repository<Item>,
+    private readonly entityManager: EntityManager,
+  ) {}
 
   async create(createItemDto: CreateItemDto) {
     const item = new Item(createItemDto);
@@ -15,19 +20,32 @@ export class ItemsService {
     return 'This action adds a new item';
   }
 
-  findAll() {
-    return `This action returns all items`;
+  async findAll() {
+    return this.itemRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: number) {
+    const item = await this.itemRepository.findOneBy({ id });
+
+    if (!item) {
+      throw new Error(`Item with id ${id} not found`);
+    }
+
+    return item;
   }
 
-  update(id: number, updateItemDto: UpdateItemDto) {
+  async update(id: number, updateItemDto: UpdateItemDto) {
+    const item = await this.itemRepository.findOneBy({ id });
+    if (!item) {
+      throw new Error(`Item with id ${id} not found`);
+    }
+    item.public = updateItemDto.public;
+    await this.entityManager.save(item);
     return `This action updates a #${id} item`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
+    await this.itemRepository.delete(id);
     return `This action removes a #${id} item`;
   }
 }
