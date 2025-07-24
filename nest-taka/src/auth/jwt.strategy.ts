8da2fe07d259a865c6fc3222ec71280a842895f9
HaiBV <buivuhai.bk@gmail.com>
2025-07-24 22:34:46 +0700

    chore: update cookieExtractor

--- nest-taka/src/auth/jwt.strategy.ts
+++ nest-taka/src/auth/jwt.strategy.ts
@@ -3,24 +3,21 @@ import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { PassportStrategy } from '@nestjs/passport';
 
+const cookieExtractor = (req: Request): string | null => {
+  if (req.cookies && 'token' in req.cookies) {
+    return req.cookies.token as string;
+  }
+  return null;
+};
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        JwtStrategy.extractJWT,
-        // ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ]),
+      jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
       // TODO: use config
       secretOrKey: 'secret',
     });
-  }
-
-  private static extractJWT(req: Request): string | null {
-    if (req.cookies && 'token' in req.cookies) {
-      return req.cookies.token;
-    }
-    return null;
   }
 
   validate(payload: { id: string; email: string }) {
