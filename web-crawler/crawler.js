f5ec6e28437e8ab5f7c940fc892c71569144c989
HaiBV <buivuhai.bk@gmail.com>
2026-06-21 22:09:23 +0700

    chore: update dotenv

--- web-crawler/crawler.js
+++ web-crawler/crawler.js
@@ -1,11 +1,20 @@
 const { crawlCategories } = require("./components/crawlCategories");
 const { crawlPosts } = require("./components/crawlPosts");
+require('dotenv').config()
 
 // define a crawler function
 const crawler = async () => {
-  const { postsUrlToVisit } = await crawlCategories(["https://truyenfull.today/the-loai/trong-sinh/hoan/"]);
+  const categories = (process.env.CATEGORIES?.split(",") || []).filter((category) => category.trim() !== "" && category.match(/https?:\/\/(www\.)?truyenfull\.today\/the-loai\/.+/));
+  if (categories.length === 0) {
+    console.error("No categories provided. Please set the CATEGORIES environment variable.");
+    console.log(`Categories: ${process.env.CATEGORIES}`)
+    return;
+  }
+  console.log({ categories });
+
+  const { postsUrlToVisit } = await crawlCategories(categories);
   console.log(`Found ${postsUrlToVisit.length} posts to visit.`);
-  // console.log({ postsUrlToVisit });
+  console.log({ postsUrlToVisit });
 
   await crawlPosts(postsUrlToVisit);
 
